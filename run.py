4fe1fc692b825db570ecea9416b0cbb00ee7bf10
Philipp Tschannen <phitsc@gmail.com>
2018-10-16 23:20:19 +0200

    Add run.py. Does not work fully yet

---
+++ run.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+from helpers import docker_image_exists
+from pathlib import Path
+from subprocess import call
+
+project_name="check-cpp-api"
+
+def main():
+    if (docker_image_exists(project_name)):
+
+        # extract test project name and directory from -p argument
+        parser = argparse.ArgumentParser(add_help=False)
+        parser.add_argument('-p')
+        args, unknownArgs = parser.parse_known_args()
+
+        test_project_dir = Path(args.p).parent if args.p else None
+        test_project_name = Path(args.p).parent.stem if args.p else None
+
+        # project_volume required to run docker-run.sh script
+        project_volume = ("{}:/root/clang-llvm/llvm/tools/clang/tools/extra/{}"
+            .format(Path.cwd().as_posix(), project_name))
+
+        if test_project_dir:
+            test_project_volume = ("{}:/root/test_project/{}"
+                .format(test_project_dir.as_posix(), test_project_name))
+        else:
+            # if -p was not supplied, just pass same volume twice
+            # to specify a valid volume
+            test_project_volume = project_volume
+
+        call([
+            'docker',
+            'run',
+            '-it', # use container interactively
+            '--rm', # remove container when it exits
+            '-v', project_volume,
+            '-v', test_project_volume,
+            project_name, # project name = image name
+            "/root/clang-llvm/llvm/tools/clang/tools/extra/{}/Docker/docker-run.sh"
+            .format(project_name),
+            " ".join(sys.argv[1:]) # pass on command line arguments
+            # "/bin/bash"
+        ])
+
+    else:
+        raise Exception("{} Docker image not found. Run make.py first."
+            .format(project_name)
+        )
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+       print("Error:", e)
